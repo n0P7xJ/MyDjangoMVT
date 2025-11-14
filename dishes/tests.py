091e8565a3e1ca7daff493ce2ae5b242ae803c72
n0P7xJ <oleh@DESKTOP-P1DPKAL.localdomain>
2025-11-14 20:11:01 +0100

    Create Category & DB

--- dishes/tests.py
+++ dishes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_str_method(self):
+        category = Category.objects.create(name="Салати")
+        self.assertEqual(str(category), "Салати")
+
