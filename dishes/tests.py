5c079106b0a23d9ce7027741d68557bae9ae988d
n0P7xJ <oleh@DESKTOP-P1DPKAL.localdomain>
2025-11-30 17:18:04 +0100

    Fix bug

--- dishes/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Category
-
-class CategoryModelTest(TestCase):
-    def test_str_method(self):
-        category = Category.objects.create(name="Салати")
-        self.assertEqual(str(category), "Салати")
-
